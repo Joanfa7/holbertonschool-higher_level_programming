6d875c6e2cdba4a51f00a5ef5e3fabefba000d31
Joanfa7 <jafabre95@gmail.com>
2022-04-26 18:57:17 +0000

    adding new file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num1 in range(0, 9):
     for num2 in range(0, 10):
-        if num2 >= num1 and num1 != num2:
-            print("{}{}, ".format(num1, num2), end='')
-        elif num1 == 8 and num2 == 9:
+        if num1 == 8 and num2 == 9:
             print("{}{}".format(num1, num2))
+        elif num2 > num1 and num1 != num2:
+            print("{}{}, ".format(num1, num2), end='')
