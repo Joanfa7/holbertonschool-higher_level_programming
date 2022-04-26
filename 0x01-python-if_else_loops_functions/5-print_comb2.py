ca8822e4c7e5cec02259f847a7770fd08a155d69
Joanfa7 <jafabre95@gmail.com>
2022-04-26 17:38:26 +0000

    adding new file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    print("{:02d}, ".format(number), end='')
-print('')
+for number in range(0,100):
+    if number != 99:
+        print("{:02d}, ".format(number), end='')
+
+    elif number == 99:
+        print("{}".format(number))
