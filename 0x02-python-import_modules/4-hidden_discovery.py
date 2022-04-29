6db594aceb35a8ee55847f559b31ad9851256bef
Joanfa7 <jafabre95@gmail.com>
2022-04-29 00:38:03 +0000

    adding file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
+<<<<<<< Updated upstream
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
             print(name)
+=======
+>>>>>>> Stashed changes
