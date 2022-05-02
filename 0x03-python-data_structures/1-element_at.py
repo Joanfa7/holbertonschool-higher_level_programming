f6bf5a902d81aa5351a833df1cdcc4ca3dd7fa50
Joanfa7 <jafabre95@gmail.com>
2022-05-02 19:46:10 +0000

    fix bug

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,9 @@
 def element_at(my_list, idx):
     for ctr in my_list:
         ctr += 1
-    if idx < 0 or idx > ctr:
+    if idx < 0:
+        return None
+    elif idx > ctr:
         return None
     else:
         return idx + 1
