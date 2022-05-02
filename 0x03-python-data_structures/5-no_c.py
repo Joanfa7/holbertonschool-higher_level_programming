455558c90d2f59b29dbb8993cb022c8478b0ddf5
Joanfa7 <jafabre95@gmail.com>
2022-05-02 22:14:23 +0000

    fix bug

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    char_to_replace = { 'c' : '',
-                        'C' : ''}
+    char_to_replace = {'c': '',
+                       'C': ''}
     result = ''
     for elem in my_string:
         if elem in char_to_replace:
