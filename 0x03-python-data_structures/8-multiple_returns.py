f378eb6b8c4c564979ebed80699867b6b3fa4c19
Joanfa7 <jafabre95@gmail.com>
2022-05-03 02:05:30 +0000

    fix bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     char1 = sentence[0]
     if length == 0:
-        return None
+        return [length, None]
     else:
         return [length, char1]
