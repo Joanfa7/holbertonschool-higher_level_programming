11f9124c5fa028c3b2d28848596573f1480d7036
Joanfa7 <jafabre95@gmail.com>
2022-05-05 02:54:06 +0000

    fix bug

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_list = a_dictionary.copy()
     for value in new_list:
-         new_list[value] = new_list[value] * 2
+        new_list[value] = new_list[value] * 2
     return(new_list)
