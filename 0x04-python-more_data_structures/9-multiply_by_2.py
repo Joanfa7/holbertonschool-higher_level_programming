a4a436cd309a3e5a926941b23a273b40a019ea98
Joanfa7 <jafabre95@gmail.com>
2022-05-05 02:51:59 +0000

    adding new file

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_list = a_dictionary.copy()
+    for value in new_list:
+         new_list[value] = new_list[value] * 2
+    return(new_list)
