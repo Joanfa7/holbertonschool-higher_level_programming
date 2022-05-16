89620e77a6d3f4c5405439375bf84cb2a0d8abed
Joanfa7 <jafabre95@gmail.com>
2022-05-16 17:56:59 +0000

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             ctr += 1
-        except:
+        except TypeError:
             continue
 
     print()
