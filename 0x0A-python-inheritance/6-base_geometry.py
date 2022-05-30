d7c93af155e369d079ed8d44f8948e80f7657da1
Joanfa7 <jafabre95@gmail.com>
2022-05-30 16:40:59 +0000

    adding new file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates an empty class"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
