bc4dfc2c2eebab36cc3cbda4aef4f934074344e2
Joanfa7 <jafabre95@gmail.com>
2022-06-01 19:24:13 +0000

    adding new file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" funccion that appends a new text to a file """
+
+
 def append_write(filename="", text=""):
+    """ opwn a file, append method, using utf-8 
+        write a text and return the len of the text appended"""
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(text)
         return len(text)
