68ba948a45146dbdfe9113734260e06bffa90c43
Joanfa7 <jafabre95@gmail.com>
2022-06-02 01:49:47 +0000

    adding new file

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module of an class object """
+
+
+class Student:
+    """ Defining student class """
+
+
+    def __init__(self, first_name, last_name, age):
+        """ student public atributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """ a json object """
+        return self.__dict__
