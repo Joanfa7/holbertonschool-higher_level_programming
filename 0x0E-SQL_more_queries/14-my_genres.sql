8aeb22ff62a6821ea8e40ba626dea229f4729966
Joanfa7 <jafabre95@gmail.com>
2022-07-08 11:18:30 +0000

    fix bug 14-file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- imported database
 -- scripts lists all generes of a database
-SELECT tv_genres.name 
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.shows_id
+INNER JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
