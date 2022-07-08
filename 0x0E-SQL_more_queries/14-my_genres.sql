28072f3b2e7f0a32c4c9a441bc1d2e1426ca97b6
Joanfa7 <jafabre95@gmail.com>
2022-07-08 01:07:30 +0000

    fix typo of 14-file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
+-- imported database
 -- scripts lists all generes of a database
 SELECT tv_genres.name 
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_show ON tv_show.id = tv_show_genres.show_id
-WHERE tv_show.title='Dexter'
+INNER JOIN tv_shows ON tv_show.id = tv_show_genres.shows_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
