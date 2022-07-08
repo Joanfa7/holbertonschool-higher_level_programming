4d0ffe8eb9898c0c86b5924f643520fb5172c52e
Joanfa7 <jafabre95@gmail.com>
2022-07-08 11:07:35 +0000

    fix bug 12-file

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,5 @@
 -- Script lists all shows in the data base
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_show_generes.show_id IS NULL
-ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
+WHERE tv_show_generes.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
