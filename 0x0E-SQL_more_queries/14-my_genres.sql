f24fe628087247a1a4d01bbe97105f1a164428ef
Joanfa7 <jafabre95@gmail.com>
2022-07-08 00:59:57 +0000

    Adding 14-file

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- scripts lists all generes of a database
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genere_id
+INNER JOIN tv_show ON tv_show.id=tv_show_genres.show_id
+WHERE tv_show.tittle='Dexter'
+ORDER BY tv_genres.name ASC
