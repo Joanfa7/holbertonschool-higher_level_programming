20cd7f19dae34831cc1e65863cfbd6b325493fd5
Joanfa7 <jafabre95@gmail.com>
2022-07-08 11:14:23 +0000

    fix bug 13-file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script lists all genres
-SELECT tv_genres.name AS genre, 
-COUNT(*) AS number_of_shows 
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows 
 FROM tv_show_genres
 JOIN tv_show_genres ON tv_generes.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
