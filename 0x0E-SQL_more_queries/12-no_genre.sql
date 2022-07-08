1771fb5d15af20d04a227d929978acaae5a0cd67
Joanfa7 <jafabre95@gmail.com>
2022-07-08 00:08:32 +0000

    add 12-file

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Src
+-- Script lists all shows in the data base
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_generes.show_id IS NULL
+ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
