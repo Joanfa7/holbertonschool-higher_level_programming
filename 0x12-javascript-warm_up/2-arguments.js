7234e895d6b0f43fe5d14b4a5142941c6cf872b3
Joanfa7 <jafabre95@gmail.com>
2022-08-01 18:20:31 +0000

    adding new file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@
 const argc = process.argv.length;
 if (argc <= 2) {
   console.log('No argument');
+} else if (argc > 3) {
+  console.log('Arguments found');
 } else {
   console.log('Argument found');
 }
