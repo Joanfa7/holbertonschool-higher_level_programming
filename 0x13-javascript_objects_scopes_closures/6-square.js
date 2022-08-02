077f0c5f1a8b8174a44968ea87a2112f80a44f78
Joanfa7 <jafabre95@gmail.com>
2022-08-02 19:47:40 +0000

    adding file 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,20 +2,20 @@
 const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
-	constructor (size) {
-		super(size, size);
-	}
-	charPrint (c){
-		if (typeof c === 'undefined'){
-			this.print();
-		} else {
-			let i, j;
-			for (i = 0; i < this.height; i++){
-				for (j = 0; j < this.width; j++) { process.stdout.write('X'); }
-				console.log('');
+  constructor (size) {
+    super(size, size);
+  }
 
-			}
-		}
-	}
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      let i, j;
+      for (i = 0; i < this.height; i++) {
+        for (j = 0; j < this.width; j++) { process.stdout.write(c); }
+        console.log('');
+      }
+    }
+  }
 }
 module.exports = Square;
