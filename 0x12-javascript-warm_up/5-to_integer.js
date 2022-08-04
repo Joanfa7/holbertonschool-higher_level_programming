6a8ef44f41314f4348ebc67c71071011be37dc1e
Joanfa7 <jafabre95@gmail.com>
2022-08-04 00:49:51 +0000

    adding new file

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const args1 = process.argv[2];
-
-if (Number(args1)) {
-  console.log('My number: ', args1);
-} else {
-  console.log('No a number');
+if (Number(args1)){
+	console.log('My number: ', );
+}else{
+	console.log('Not a number');
 }
