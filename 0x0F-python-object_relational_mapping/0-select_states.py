87066bb44c7956b7086afe80733e5336f9951fb3
Joanfa7 <jafabre95@gmail.com>
2022-08-04 19:04:26 +0000

    adding 0-file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that lists all states form the data base hbtn_0e_0_usa"""
+
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", user="root", password="root",
+                         database="hbtn_0e_0_usa")
+
+    cursor = db.cursor()
+
+    cursor.execute("Select * FROM states ORDER BY states.id ASC")
+
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
