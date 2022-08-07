5340d1d702501dde8c57177266e4fd520cc7128c
Joanfa7 <jafabre95@gmail.com>
2022-08-07 22:21:47 +0000

    fix typo on model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-""" Module """
+
+"""
+Module
+"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """ Class state inheritent form Base """
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
