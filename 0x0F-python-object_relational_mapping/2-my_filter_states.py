816086678823181c94b9385608b33d66afe3e13e
Joanfa7 <jafabre95@gmail.com>
2022-08-07 22:27:02 +0000

    adding if statement in 2-file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     result = cursor.fetchall()
 
     for row in result:
-        print(row)
+        if row[1] == find_name:
+            print(row)
     cursor.close()
     db.close()
