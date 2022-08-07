144b345eadad876e6eaa6dde66101b211608ae5c
Joanfa7 <jafabre95@gmail.com>
2022-08-07 19:23:52 +0000

    fixing pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ script that lists all states form the data base hbtn_0e_0_usa"""
 
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user="root", password="root",
-                         database="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
     cursor = db.cursor()
 
@@ -17,5 +18,4 @@ if __name__ == "__main__":
     for row in result:
         print(row)
     cursor.close()
-    connector.close()
-
+    db.close()
