cd16b4e99e657137f8e98f178ed7ab633a57a1ad
Joanfa7 <jafabre95@gmail.com>
2022-08-07 22:13:33 +0000

    adding new file

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Class state inheritent form Base """
+    __tablename__ = 'states'
+    id = Column(Integer, nulltable=False, primary_key=True)
+    name =  Column(String(128), nullable=False)
