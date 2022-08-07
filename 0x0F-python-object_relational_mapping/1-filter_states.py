762f13995818688843f109175293f05e22e4e2a0
Joanfa7 <jafabre95@gmail.com>
2022-08-07 19:27:44 +0000

    fixing pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """ script that lists all states form the data base hbtn_0e_0_usa"""
 
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user="root", password="root",
-                         database="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
     cursor = db.cursor()
 
-    cursor.execute("Select * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute("Select * FROM states WHERE name LIKE 'N%' ORDER\
+                    BY states.id ASC")
 
     result = cursor.fetchall()
 
     for row in result:
         print(row)
-    curson.close()
-    connector.close()
+    cursor.close()
+    db.close()
