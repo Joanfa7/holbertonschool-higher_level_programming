e78c6aa8a8d017b8a7e8138ed05e5ad318dace01
Joanfa7 <jafabre95@gmail.com>
2022-08-07 19:34:35 +0000

    adding if statement to evaluate N is the fists char

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ script that lists all states form the data base hbtn_0e_0_usa"""
 
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
 
-    find_name = sys.argv[4]
+    find_name = argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user="root", password="root",
-                         database="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
     cursor = db.cursor()
 
-    cursor.execute('Select * FROM states WHERE name LIKE "{}%" ORDER BY states.id ASC'
+    cursor.execute('Select * FROM states WHERE name LIKE "{}%"\
+                   ORDER BY states.id ASC'
                    .format(find_name))
 
     result = cursor.fetchall()
@@ -21,5 +22,4 @@ if __name__ == "__main__":
     for row in result:
         print(row)
     cursor.close()
-    connector.close()
-
+    db.close()
