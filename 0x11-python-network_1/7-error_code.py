bf8790742a11bd1a03875a9c6c11ea183bcbab70
Joanfa7 <jafabre95@gmail.com>
2022-08-21 20:42:55 +0000

    adding new file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetch an URL data """
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    reque = requests.get(argv[1])
+    if reque.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
