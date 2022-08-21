6bdaa899238e991fe39a9cd29d8320b9baeaf4ea
Joanfa7 <jafabre95@gmail.com>
2022-08-21 20:30:14 +0000

    adding new file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetch an URL data """
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    reque = requests.get('argv[1]')
+    print(r.headers.get('X-Request-Id'))
