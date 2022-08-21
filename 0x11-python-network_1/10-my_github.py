a291b4786b591aea60ac20434c73003097acbd6a
Joanfa7 <jafabre95@gmail.com>
2022-08-21 21:08:29 +0000

    Adding new file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,4 +9,4 @@ import requests
 if __name__ == "__main__":
     url = "http://api.github/user"
     r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
-    rint(r.json().get('id'))
+    print(r.json().get('id'))
