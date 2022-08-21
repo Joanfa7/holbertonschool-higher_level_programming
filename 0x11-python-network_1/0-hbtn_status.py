1d4aa63b65e320e6ef518fd4d1a601b90f91e787
Joanfa7 <jafabre95@gmail.com>
2022-08-21 18:33:44 +0000

    fix typo on file 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Fetch an URL data """
-from urllib import request
+import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.urlopen('https://intranet.hbtn.io/status') as respo:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as respo:
         data = respo.read()
         print("Body response:")
         print("    - type: {}".format(type(data)))
         print("    - content: {}".format(type(data)))
-        print("    - utf8 content: {}".format(type(data.decode("UTF-8")))
+        print("    - utf8 content: {}".format(type(data.decode("UTF-8"))))
+
