7c9234a3a7e76014aabac237bf2a08b7eceac8c1
Joanfa7 <jafabre95@gmail.com>
2022-08-21 18:40:45 +0000

    fix typo on file 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as respo:
         data = respo.read()
         print("Body response:")
-        print("    - type: {}".format(type(data)))
-        print("    - content: {}".format(data))
-        print("    - utf8 content: {}".format(data.decode("utf-8")))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
 
