1688941c52f6788192c28719945f9091c02fbaff
Joanfa7 <jafabre95@gmail.com>
2022-09-13 17:10:51 +0000

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-
 const process = require('process');
 const fs = require('fs');
 
-var file = process.argv[2]
+const file = process.argv[2];
 
-
-
-fs.readFile(file, 'utf8', function(err, data){
-if (err) {
-	return console.error(err);
-}
-console.log(data.toString());
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
 });
