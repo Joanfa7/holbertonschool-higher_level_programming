4668c5a575d94a9cee74f91dec2650f3f1e961cb
Joanfa7 <jafabre95@gmail.com>
2022-09-13 23:13:37 +0000

    adding new file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+
+const movie = process.argv[2];
+
+const URL = 'https://swapi-api.hbtn.io/api/films/'
+
+request(URL + movie, function (error, response, body) {
+	if (error){
+		console.error(error);
+	}
+	console.log(JSON.parse(body).title);
+});
