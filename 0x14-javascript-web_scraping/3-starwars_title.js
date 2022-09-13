9511593424ea5ea9942a7c5ed5bd37918679199c
Joanfa7 <jafabre95@gmail.com>
2022-09-13 23:14:28 +0000

    adding new file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,11 @@ const process = require('process');
 
 const movie = process.argv[2];
 
-const URL = 'https://swapi-api.hbtn.io/api/films/'
+const URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(URL + movie, function (error, response, body) {
-	if (error){
-		console.error(error);
-	}
-	console.log(JSON.parse(body).title);
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
