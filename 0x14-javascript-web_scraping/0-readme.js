0204a33bc8d8fa070c0b63958a53e30918e8ca5d
Joanfa7 <jafabre95@gmail.com>
2022-09-13 17:06:47 +0000

    adding new file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs');
+
+var file = process.argv[2]
+
+
+
+fs.readFile(file, 'utf8', function(err, data){
+if (err) {
+	return console.error(err);
+}
+console.log(data.toString());
+});
