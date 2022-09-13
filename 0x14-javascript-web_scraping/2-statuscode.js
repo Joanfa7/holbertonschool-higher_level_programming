a41f4f29d6fbabeed21afaa3cc665c8ba06dd6a1
Joanfa7 <jafabre95@gmail.com>
2022-09-13 18:01:19 +0000

    add new file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,13 +2,19 @@
 const process = require('process');
 const axios = require('axios').default;
 
+
+
 const URL = process.argv[2];
 
 axios.get(URL)
-  .then(function (response) {
-    for (const key in response) {
-      if (key === 'status') {
-        console.log(`code: ${response[key]}`);
-      }
-    }
-  });
+	.then(function (response) {
+		for (const key in response) {
+			if (key === 'status') {
+				console.log(`code: ${response[key]}`);
+			}
+		}
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error.response.status);
+	});
